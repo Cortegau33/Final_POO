eca8902c6556b9ba4ed236190c3a7350b2699de2
Cortegau33 <couchin27@gmail.com>
2024-09-23 16:57:29 -0500

    segunda sesion
    segundo comiit

--- gestion_ventas/inventario/admin.py
+++ gestion_ventas/inventario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categoria, Proveedor, Producto, Cliente, Venta
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Proveedor)
+admin.site.register(Producto)
+admin.site.register(Cliente)
+admin.site.register(Venta)
--- gestion_ventas/inventario/views.py
+++ gestion_ventas/inventario/views.py
@@ -1,5 +1,6 @@
-import csv
-from django.shortcuts import render, redirect, get_object_or_404
-from django.utils.dateparse import parse_date
-from django.http import HttpResponse
-from .models import Categoria, Proveedor, Producto, Cliente, Venta
+from django.shortcuts import render, get_object_or_404
+from .models import Producto
+
+def lista_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'ventas/lista_productos.html', {'productos': productos})
